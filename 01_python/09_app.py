8b8dc88775609ca48878d129d9f1d2a5ff93eb34
Alex Nikitin <nktn.lx@gmail.com>
2024-10-22 08:33:01 +0300

    all tasks finished

--- 01_python/09_app.py
+++ 01_python/09_app.py
@@ -1,10 +1,14 @@
 import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Depends
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 app = FastAPI()
+
 
 # task 1
 # @app.get('/')
@@ -36,3 +40,123 @@ class User(BaseModel):
 @app.post('/user/validate')
 def validate_user(user: User):
     return f'Will add user: {user.name} {user.surname} with age {user.age}'
+
+
+# task 6
+# @app.get('/user/{id}')
+# def get_user(id: int):
+#     conn = psycopg2.connect(
+#         "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml",
+#         cursor_factory=RealDictCursor
+#     )
+#     cursor = conn.cursor()
+#     query = '''
+#         SELECT
+#           gender,
+#           age,
+#           city
+#         FROM
+#           "user"
+#         WHERE
+#           id = %s
+#         LIMIT
+#           1
+#         '''
+#     cursor.execute(query, (id,))
+#
+#     result = cursor.fetchone()
+#     return result
+
+
+# task 7
+# @app.get('/user/{id}')
+# def get_user(id: int):
+#     conn = psycopg2.connect(
+#         "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml",
+#         cursor_factory=RealDictCursor
+#     )
+#     cursor = conn.cursor()
+#     query = '''
+#         SELECT
+#           gender,
+#           age,
+#           city
+#         FROM
+#           "user"
+#         WHERE
+#           id = %s
+#         LIMIT
+#           1
+#         '''
+#     cursor.execute(query, (id,))
+#
+#     result = cursor.fetchone()
+#
+#     if not result:
+#         raise HTTPException(404, 'user not found')
+#     return result
+
+
+#task 8
+def get_db():
+    conn = psycopg2.connect(
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml",
+        cursor_factory=RealDictCursor
+    )
+    return conn
+
+
+@app.get('/user/{id}')
+def get_user(id: int, db=Depends(get_db)):
+    with db.cursor() as cursor:
+        query = '''
+            SELECT
+              gender,
+              age,
+              city
+            FROM 
+              "user"
+            WHERE 
+              id = %s
+            LIMIT 
+              1
+            '''
+        cursor.execute(query, (id,))
+        result = cursor.fetchone()
+
+    if not result:
+        raise HTTPException(404, 'user not found')
+    return result
+
+
+# task 9
+class PostResponse(BaseModel):
+    id: int
+    text: str
+    topic: str
+
+    class Config:
+        orm_mode=True
+
+
+@app.get('/post/{id}', response_model=PostResponse)
+def get_post_info(id: int, db=Depends(get_db)) -> PostResponse:
+    with db.cursor() as cursor:
+        query = '''
+            SELECT
+              id,
+              text,
+              topic
+            FROM 
+              "post"
+            WHERE 
+              id = %s
+            LIMIT 
+              1
+            '''
+        cursor.execute(query, (id,))
+        result = cursor.fetchone()
+
+    if not result:
+        raise HTTPException(404, 'post not found')
+    return PostResponse(**result)
