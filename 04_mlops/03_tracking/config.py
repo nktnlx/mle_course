e6f6af36fb1e9c0c3d463821d1e254068f77f12c
Alex Nikitin <nktn.lx@gmail.com>
2025-08-12 21:10:46 +0500

    create config.py

---
+++ 04_mlops/03_tracking/config.py
@@ -0,0 +1,16 @@
+config = {
+    "random_state": 42,
+    "data": {
+        "test_size": 0.2,
+    },
+    "logistic_regression": {
+        "max_iter": 200,
+        "penalty": "l2",
+        "multi_class": "auto",
+    },
+    "decision_tree": {
+        "max_depth": 10,
+        "max_leaf_nodes": 5,
+        "criterion": "entropy",
+    }
+}
